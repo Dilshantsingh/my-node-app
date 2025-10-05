7aaf48e7ebc85d10d8f5cebe5248246b2b0450a2
Dilshantsingh <Dilshant2004@gmail.com>
2025-10-05 19:38:37 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from my Node.js app running on GitHub deployment!");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
